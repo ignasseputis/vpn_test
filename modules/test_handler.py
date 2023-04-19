a265b2821536b3cdcd31a581d2b1b0b16dfd5423
ignasseputis <seputisignas@gmail.com>
2023-04-19 09:28:17 +0300

    data output changed to one file

--- modules/test_handler.py
+++ modules/test_handler.py
@@ -17,78 +17,92 @@ class TestHandler:
         
         
     def TestVPN(self):
+        self.csv.PrepareCSV(self.arguments, self.instanceServer, self.instanceClient)
+        infoDict={"connectionType":"","authenticationType":"","protocol":"","lzo":"",
+                  "encryptionProtocol":"","tlsCipher":"", "authenticationAlgorithm":"",
+                  "additionalHMACAuthentication":""}
         for connection in self.arguments.connection_types:
             self.instanceServer.ChangeLAN(connection)
+            newInfoDict=infoDict|{"connectionType":connection}
             infoLine="Connection type: {0}".format(connection)
-            self.SetAuthenticationConfig(connection, infoLine)
+            self.SetAuthenticationConfig(connection, infoLine, newInfoDict)
+        self.SendFile()
         
 
-    def SetAuthenticationConfig(self, connection, infoLineOld):
+    def SetAuthenticationConfig(self, connection, infoLineOld, infoDict):
         for auth in self.arguments.auth_types:
             serverConfig={}
             clientConfig={}
+            newInfoDict=infoDict|{"authenticationType":auth}
             infoLine=infoLineOld+", Authentication type: "+auth 
-            self.SetProtocolConfig(connection, auth, serverConfig, clientConfig, infoLine)
+            self.SetProtocolConfig(connection, auth, serverConfig, clientConfig, infoLine, newInfoDict)
 
-    def SetProtocolConfig(self, connection, authentication, serverConfig, clientConfig, infoLineOld):
+    def SetProtocolConfig(self, connection, authentication, serverConfig, clientConfig, infoLineOld, infoDict):
         for protocol in self.arguments.protocols:
+            newInfoDict=infoDict|{"protocol":protocol["server"]["proto"][0:3]}
             infoLine=infoLineOld+", Protocol: "+protocol["server"]["proto"]
             newServerConfig=serverConfig | protocol["server"]
             newClientConfig=clientConfig | protocol["client"]
-            self.SetLZOConfig(connection, authentication, infoLine, newServerConfig, newClientConfig)
+            self.SetLZOConfig(connection, authentication, infoLine, newServerConfig, newClientConfig, newInfoDict)
 
-    def SetLZOConfig(self, connection, authentication, infoLineOld, serverConfig, clientConfig):
+    def SetLZOConfig(self, connection, authentication, infoLineOld, serverConfig, clientConfig, infoDict):
         for lzo in self.arguments.lzo:
+            newInfoDict=infoDict|{"lzo":lzo["comp_lzo"]}
             infoLine=infoLineOld+", LZO: "+lzo["comp_lzo"]
             newServerConfig=serverConfig | lzo
             newClientConfig=clientConfig | lzo
-            self.SetEncryptionConfig(connection, authentication, infoLine, newServerConfig, newClientConfig)
+            self.SetEncryptionConfig(connection, authentication, infoLine, newServerConfig, newClientConfig, newInfoDict)
 
-    def SetEncryptionConfig(self, connection, authentication, infoLineOld, serverConfig, clientConfig):
+    def SetEncryptionConfig(self, connection, authentication, infoLineOld, serverConfig, clientConfig, infoDict):
         match authentication:
             case "psk":
                 encryptionProtocols=self.arguments.psk_encryption_types
             case "tls" | "tls_pwd" | "pwd":
                 encryptionProtocols=self.arguments.encryption_types
         for encryption in encryptionProtocols:
+            newInfoDict=infoDict|{"encryptionProtocol": encryption["cipher"]}
             infoLine=infoLineOld+", Encryption protocol: "+encryption["cipher"]
             newServerConfig=serverConfig | encryption
             newClientConfig=clientConfig | encryption
             match authentication:
                 case "psk":
-                    self.InitiateTest(connection, authentication, infoLine, newServerConfig, newClientConfig)
+                    self.InitiateTest(connection, authentication, infoLine, newServerConfig, newClientConfig, newInfoDict)
                 case "tls" | "tls_pwd":
-                    self.SetTLSCipherConfig(connection, authentication, infoLine, newServerConfig, newClientConfig)
+                    self.SetTLSCipherConfig(connection, authentication, infoLine, newServerConfig, newClientConfig, newInfoDict)
                 case "pwd":
-                    self.SetAuthAlgoConfig(connection, authentication, infoLine, newServerConfig, newClientConfig)
+                    self.SetAuthAlgoConfig(connection, authentication, infoLine, newServerConfig, newClientConfig, newInfoDict)
 
-    def SetTLSCipherConfig(self, connection, authentication, infoLineOld, serverConfig, clientConfig):
+    def SetTLSCipherConfig(self, connection, authentication, infoLineOld, serverConfig, clientConfig, infoDict):
         for TLSCipher in self.arguments.tls_cipher:
             if(TLSCipher["_tls_cipher"]=="all"):
+                newInfoDict=infoDict|{"tlsCipher": TLSCipher["_tls_cipher"]}
                 infoLine=infoLineOld+", TLS cipher: "+TLSCipher["_tls_cipher"]
             else:
+                newInfoDict=infoDict|{"tlsCipher": TLSCipher["tls_cipher"][0]}
                 infoLine=infoLineOld+", TLS cipher: "+TLSCipher["tls_cipher"][0]
 
             newServerConfig=serverConfig | TLSCipher
             newClientConfig=clientConfig | TLSCipher
-            self.SetAuthAlgoConfig(connection, authentication, infoLine, newServerConfig, newClientConfig)
+            self.SetAuthAlgoConfig(connection, authentication, infoLine, newServerConfig, newClientConfig, newInfoDict)
 
-    def SetAuthAlgoConfig(self, connection, authentication, infoLineOld, serverConfig, clientConfig):
+    def SetAuthAlgoConfig(self, connection, authentication, infoLineOld, serverConfig, clientConfig, infoDict):
         for auth in self.arguments.authentication_algorithms:
+            newInfoDict=infoDict| {"authenticationAlgorithm":auth["auth"]}
             infoLine=infoLineOld+", Authentication algorithm: " + auth["auth"]
             newServerConfig=serverConfig | auth
             newClientConfig=clientConfig | auth
-            self.SetHMACConfig(connection, authentication, infoLine, newServerConfig, newClientConfig)
+            self.SetHMACConfig(connection, authentication, infoLine, newServerConfig, newClientConfig,newInfoDict)
 
-    def SetHMACConfig(self, connection, authentication, infoLineOld, serverConfig, clientConfig):
+    def SetHMACConfig(self, connection, authentication, infoLineOld, serverConfig, clientConfig, infoDict):
         for hmac in self.arguments.hmac_authentication:
+            newInfoDict=infoDict|{"additionalHMACAuthentication": hmac["_tls_auth"]}
             infoLine=infoLineOld+", Additional HMAC authentication: " + hmac["_tls_auth"]
             newServerConfig=serverConfig | hmac
             newClientConfig=clientConfig | hmac 
-            self.InitiateTest(connection, authentication, infoLine, newServerConfig, newClientConfig, True)
+            self.InitiateTest(connection, authentication, infoLine, newServerConfig, newClientConfig, newInfoDict, True)
 
-    def InitiateTest(self, connection, authentication, infoLine, serverConfig, clientConfig, hmac=False):
-        
+    def InitiateTest(self, connection, authentication, infoLine, serverConfig, clientConfig, newInfoDict, hmac=False):
+        parameters=list(newInfoDict.values())
         newClientConfig=clientConfig|{"remote": self.instanceServer.url}
 
         print("Setting the configuration to:\n{0}\n".format(infoLine))
@@ -102,9 +116,9 @@ class TestHandler:
         sleep(5)
 
         speedTest=SpeedTest(server=self.instanceServer, client=self.instanceClient, output=self.outFile, csv=self.csv)
-        speedTest.InitiateSpeedtest(infoLine, self.arguments.test_count, self.arguments.test_length, connection)
+        speedTest.InitiateSpeedtest(infoLine, self.arguments.test_count, self.arguments.test_length, connection, parameters)
 
-        self.SendFile()
+        #self.SendFile()
 
         print(infoLine)
         print("\n\n")
